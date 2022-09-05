42a9937843925132e6fa657f9e2c31dbb934b63d
Cristian Valls <Cristian@neurognos.com>
2022-09-05 15:02:54 -0300

    agregado deteccion de tipo de rangos y , rango minimo y maximo

--- main_streamlit_app.py
+++ main_streamlit_app.py
@@ -6,12 +6,38 @@ import numpy as np
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
+def get_type(cell):
+    if pd.isna(cell):
+        return ('not assign') , None , None
+    cell_string = cell.lower()
+    array_splitted = cell_string.split(' ')
+    #caso hasta:
+    if 'hasta' in array_splitted:
+        rango_min = 0
+        rango_max = array_splitted[1]
+        return 'rango_hasta' , rango_min, rango_max
+    #caso menor a:
+    if 'menor' in array_splitted:
+        rango_min = 0
+        rango_max = array_splitted[2]
+        return 'rango_menor'  , rango_min, rango_max
+    #caso min - max:
+    if len(array_splitted) == 3 and array_splitted[1] == '-':
+        rango_min = array_splitted[0]
+        rango_max = array_splitted[2]
+        return 'rango' , rango_min, rango_max
+    if 'negativo' in array_splitted:
+        return 'true-false',None,None
+    else:
+        return None,None,None
+        
+
+        
+
 uploaded_file = st.file_uploader("Choose a XLS file", type="xls")
 
 if uploaded_file:
     novus_output = pd.read_excel(uploaded_file , skiprows=1)
-        # Fecha output novuslis: 0 	2022-06-01 08:11:04
-        # fecha input tested : 06/08/2022 07:12:24
     novus_output = novus_output.rename(columns={"Valor": "result" , 'Prestación Estructura': 'nameIndicator',"Unidad": "unitMeasurement", "Fecha":"createdAt","CodFonasa":"code","Orden":"requestId", "Documento":"clientID" , "Paciente": "ClientName" , "CodInterno":"codeInternal" , "Prestacion Orden":"nameExam"})
     # import categories from each examn ( SANGRE, ORINA, etc)
 
@@ -19,6 +45,7 @@ if uploaded_file:
     # Drop columns
     novus_output = novus_output.drop(["Programa" , "Edad" , "Sexo"], axis = 1)
     novus_output = novus_output[novus_output.columns.drop(list(novus_output.filter(regex='Unnamed')))]
+
 
     # Create empty cols
     novus_output['requestStatus'] = 1
@@ -38,6 +65,11 @@ if uploaded_file:
 
     # reportar True en outofrange si analista flageo con *
     novus_output['outOfRange']  = np.where(novus_output['Estado'] == '*', True, False)
+
+    # asignar tipo de rango y valores inferiores y superiores.
+
+    novus_output['categoryIndicator'], novus_output['referenceInf'], novus_output['referenceSup'] = zip(*novus_output['Rango Ref'].apply(get_type))
+
 
 
 
